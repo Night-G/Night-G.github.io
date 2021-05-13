f48e6e2ca70a8d39e3817989e29f8e549afbdecc
Meow <61281536+Night-G@users.noreply.github.com>
2021-05-13 17:44:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.use(express.static('public'));
 // })
 
 
-// app.get("/", function (req,res) { 
-//     res.sendFile(__dirname + "/main.html")
-// });
+app.get("/", function (req,res) { 
+    res.sendFile(__dirname + "/main.html")
+});
